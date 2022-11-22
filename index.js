3c2fe56517ad126f71f9ac991d1dac592c8d9b01
Fleur41 <Muriithikings456@gmail.com>
2022-11-22 15:00:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('button', function() {
+        alert('I was clicked');
+
+    })
 }
